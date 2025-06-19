7984c0a1a5a96891d9a1e0ffc4050ea15729850c
Mike Shubrook <mikejshubrook@gmail.com>
2025-06-19 14:44:03 +0100

    added comments to IF-dyn-moms.sh script

--- scripts/IF-dyn-moms.sh
+++ scripts/IF-dyn-moms.sh
@@ -4,25 +4,27 @@
 export OMP_NUM_THREADS=4
 
 ### Numerical parameters ###
-export STEP_SIZE=0.01
-export PREC=7.0
+export STEP_SIZE=0.01 # Trotter step size, must be tested for convergence
+export PREC=7.0 # precision: 10^-{p} is the SVD threshold, must be tested for convergence
 
-### Physical parameters ###
-export GAMMA=10.0
-export W0=25.0
-export BETA=1
-export WC=5
-export ALPHA_LIST="0.02"
-export TP_LIST="1 2 3"
-export EPSMAX=25.0
-export EPS0=0.02
+### Bath parameters - required for IF.py, dynamics.py and moments.py ###
+export BETA=1 # inverse temperature
+export GAMMA=10.0 # spectral densit width
+export W0=25.0 # spectral density peak location
+export WC=5 # spectral density cutoff
+export ALPHA_LIST="0.02" # coupling strength list, can be a single value or multiple values separated by spaces
 
-### Parameters for moments calculations ###
-export S=1
-export NTH_ORDER_DERIV=4
-export MAX_DIFF_ORDER=4
+### System parameters - only required for dynamics.py and moments.py ###
+export TP_LIST="1 2 3" # list of protocol times, can be a single value or multiple values separated by spaces
+export EPSMAX=25.0 # maximum energy of the system
+export EPS0=0.02 # minimum energy of the system
 
-# Run scripts
+### Moments parameters, only required for moments.py ###
+export S=1 # equilibration time, must be tested for convergence
+export NTH_ORDER_DERIV=4 # number of derivatives to compute
+export MAX_DIFF_ORDER=4 # maximum order of the finite difference approximation for derivatives
+
+# Run scripts - comment out those that are not needed
 python ../src/IF.py
 python ../src/dynamics.py
 python ../src/moments.py
