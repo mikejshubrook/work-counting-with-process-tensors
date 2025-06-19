ba676e28267632e4c3c3f7816c07b7631bed45cd
Mike Shubrook <mikejshubrook@gmail.com>
2025-06-19 16:10:02 +0100

    moved data folder script into data folder (makes more sense)

---
+++ data/setup_data_folders.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Create the subdirectories inside data
+mkdir -p dynamics-files
+mkdir -p InfluenceFunctionals
+mkdir -p moments-files
+mkdir -p wcf-files
+mkdir -p wcf-files-combined
+
+echo "Folders created successfully."
