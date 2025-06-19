40357db282c20c230c797bd3e9ec2b7fb6c119a3
Mike Shubrook <mikejshubrook@gmail.com>
2025-06-19 15:47:17 +0100

    removed sta inputs from bash scripts as they are no longer needed

--- scripts/wcf.sh
+++ scripts/wcf.sh
@@ -23,7 +23,6 @@ export ALPHA=0.02 # coupling strength list, can be a single value or multiple va
 export TP=1.0 # list of protocol times, can be a single value or multiple values separated by spaces
 export EPSMAX=25.0 # maximum energy of the system
 export EPS0=0.02 # minimum energy of the system
-# export STA=1 # include shortcut to adiabaticity (1) or not (0)
 
 # Run scripts - comment out those that are not needed
 python ../src/wcf.py
